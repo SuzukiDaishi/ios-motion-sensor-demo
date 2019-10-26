78ced63059804127505b8d896cd008c78c62b699
SuzukiDaishi <34536327+SuzukiDaishi@users.noreply.github.com>
2019-10-27 01:01:21 +0900

    とりあえずコミット

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    let motionManager = CMMotionManager()
+    
+    var xData = [Double]()
+    var yData = [Double]()
+    var zData = [Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if motionManager.isAccelerometerAvailable {
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+                self.xData.append(data?.acceleration.x ?? 0)
+                self.yData.append(data?.acceleration.y ?? 0)
+                self.zData.append(data?.acceleration.z ?? 0)
+            }
+        }
+        
+        
     }
 
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        print(self.xData)
+        print(self.yData)
+        print(self.zData)
+    }
 }
 
